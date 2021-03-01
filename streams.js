f4e96f0a3e4d4620587f8c1c7253ce2483f8e559
Roger Bloom <rogerbloom1@gmail.com>
2021-03-01 12:29:45 -0800

    in progress

--- streams.js
+++ streams.js
@@ -1,1 +1,14 @@
 const fs = require("fs");
+const { createServer } = require ("http");
+
+
+const server = createServer((req, res) => {
+    const { url, method } = req
+    if (url === "/" && method === "GET") {
+        const readStream = fs.createReadStream(path.join(__dirname, "/index.html")
+        );
+        readStream.pipe(res);
+        return
+    };
+    res.end();node
+});

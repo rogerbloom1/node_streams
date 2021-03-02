b5f85f548d2b6c3dda045f98c6a763a4a3aebfcf
Roger Bloom <rogerbloom1@gmail.com>
2021-03-01 17:49:22 -0800

    server, pages created

--- streams.js
+++ streams.js
@@ -1,14 +1,20 @@
 const fs = require("fs");
 const { createServer } = require ("http");
-
+const { join } = require("path");;
 
 const server = createServer((req, res) => {
     const { url, method } = req
     if (url === "/" && method === "GET") {
-        const readStream = fs.createReadStream(path.join(__dirname, "/index.html")
-        );
+        res.setHeader("Content-Type", "text/html");
+        const readStream = fs.createReadStream(join(__dirname, "../Pages/index.html"));
         readStream.pipe(res);
-        return
+        
+    }else {
+        res.setHeader("Content-Type", "text/html");
+        const readStream = fs.createReadStream(join(__dirname, "../Pages/404.html"));
+        readStream.pipe(res);
+        
+
     };
-    res.end();node
+    
 });

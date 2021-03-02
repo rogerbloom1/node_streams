88e93a60631999cebdd331192db35706af889331
Roger Bloom <rogerbloom1@gmail.com>
2021-03-01 22:25:46 -0800

    first working version

--- streams.js
+++ streams.js
@@ -1,20 +1,20 @@
-const fs = require("fs");
+const { Console } = require("console");
+const { createReadStream } = require("fs");
 const { createServer } = require ("http");
-const { join } = require("path");;
+const { join } = require("path");
+const port = 3000;
 
 const server = createServer((req, res) => {
-    const { url, method } = req
+    const { url, method } = req;
     if (url === "/" && method === "GET") {
         res.setHeader("Content-Type", "text/html");
-        const readStream = fs.createReadStream(join(__dirname, "../Pages/index.html"));
+        const readStream = createReadStream(join(__dirname, "/Pages/index.html"));
         readStream.pipe(res);
         
     }else {
         res.setHeader("Content-Type", "text/html");
-        const readStream = fs.createReadStream(join(__dirname, "../Pages/404.html"));
+        const readStream = createReadStream(join(__dirname, "/Pages/404.html"));
         readStream.pipe(res);
-        
-
-    };
-    
+    }
 });
+server.listen(port, () => console.log("Server listening on " + port + "..."));
